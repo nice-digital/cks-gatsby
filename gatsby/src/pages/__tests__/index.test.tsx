507fef970d43ab5d663be59ded5f574e556a061f
Ian Routledge <ian.routledge@nice.org.uk>
2020-03-06 15:27:18 +0000

    Fix TS error

--- gatsby/src/pages/__tests__/index.test.tsx
+++ gatsby/src/pages/__tests__/index.test.tsx
@@ -5,13 +5,17 @@ import IndexPage from "../index";
 
 describe("index", () => {
 	it("should match snapshot", () => {
-		const { container } = render(<IndexPage />);
+		const { container } = render(
+			<IndexPage data={{ allTopics: { nodes: [] } }} />
+		);
 
 		expect(container.firstChild).toMatchSnapshot();
 	});
 
 	it("should render h1 with correct text", () => {
-		const { getByRole } = render(<IndexPage />);
+		const { getByRole } = render(
+			<IndexPage data={{ allTopics: { nodes: [] } }} />
+		);
 
 		expect(getByRole("heading")).toHaveTextContent("Index page");
 	});
