624c20ec4422b05f10a2295a13ffaefd26968b18
dylan <dylan.riley@nice.org.uk>
2020-05-05 21:48:19 +0100

    CKS-277 Add static pages

---
+++ gatsby/src/pages/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "../components/Layout/Layout";
+import { SEO } from "../components/SEO/SEO";
+
+const AboutPage: React.FC = () => {
+	return (
+		<Layout>
+			<SEO title="About" />
+			<h1>About</h1>
+		</Layout>
+	);
+};
+
+export default AboutPage;
