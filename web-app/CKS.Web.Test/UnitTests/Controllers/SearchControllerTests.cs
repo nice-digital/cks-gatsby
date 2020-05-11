85199d2984d58f6ba3427c11449e1036aff7c372
dylan <dylan.riley@nice.org.uk>
2020-05-11 19:04:20 +0100

    CKS-274 Add SearchControllerTests

--- web-app/CKS.Web.Test/UnitTests/Controllers/SearchControllerTests.cs
+++ web-app/CKS.Web.Test/UnitTests/Controllers/SearchControllerTests.cs
@@ -7,18 +7,43 @@ using CKS.Web.Controllers;
 using Moq;
 using Microsoft.Extensions.Logging;
 using NICE.Search.Common.Urls;
+using NICE.Search.Providers;
+using NICE.Search.Common.Enums;
+using NICE.Search.Common.Interfaces;
+using NICE.Search.Common.Models;
+using System.Text.Json;
 
 namespace CKS.Web.Test.UnitTests.Controllers
 {
 	public class SearchControllerTests
 	{
-		[Obsolete("Please replace this test with a real test when the search endpoint is setup")]
 		[Fact]
-		public void ASampleTestReturnsNonNullObject()
+		public void ASearchCanBePerformed()
 		{
-			//var searchController = new SearchController(Mock.Of<ILogger<SearchController>>());
+			var searchProvider = new Mock<ISearchProvider>();
+			searchProvider.Setup(sp => sp.Search(It.IsAny<ISearchUrl>())).Returns(new SearchResults());
+			var searchController = new SearchController(Mock.Of<ILogger<SearchController>>(), searchProvider.Object);
 
-			//searchController.Get(new SearchUrl()).ShouldNotBeNull();
+			var result = searchController.Get(new SearchUrl() { q = "Cancer" });
+
+			result.Value.ShouldNotBeNull();
+			result.StatusCode.ShouldBe(200);
+		}
+
+
+		[Fact]
+		public void AFailedSearchWillReturn500()
+		{
+			var searchProvider = new Mock<ISearchProvider>();
+			searchProvider.Setup(sp => sp.Search(It.IsAny<ISearchUrl>())).Returns(new SearchResults() { Failed = true});
+			var searchController = new SearchController(Mock.Of<ILogger<SearchController>>(), searchProvider.Object);
+
+			var response = searchController.Get(new SearchUrl());
+			var responseData = response.Value as SearchResults;
+
+			response.StatusCode.ShouldBe(500);
+			responseData.Failed.ShouldBeTrue();
+
 		}
 	}
 }
