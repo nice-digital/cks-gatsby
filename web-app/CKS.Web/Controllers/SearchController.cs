6572fe015c86960fab5721f474b761097b7e5581
dylan <dylan.riley@nice.org.uk>
2020-05-11 19:40:28 +0100

    CKS-274 SearchController returns JsonResult

--- web-app/CKS.Web/Controllers/SearchController.cs
+++ web-app/CKS.Web/Controllers/SearchController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NICE.Search.Common.Interfaces;
+using NICE.Search.Common.Models;
 using NICE.Search.Common.Urls;
 using System;
+using System.ComponentModel;
 
 namespace CKS.Web.Controllers
 {
@@ -20,9 +22,11 @@ namespace CKS.Web.Controllers
 		}
 
 		[HttpGet]
-		public object Get([FromQuery]SearchUrl searchUrl)
+		public JsonResult Get([FromQuery]SearchUrl searchUrl)
 		{
 			var search = _provider.Search(searchUrl);
+			int statusCode = 200;
+
 			if (search.Failed)
 			{
 				var logId = Guid.NewGuid().ToString("N").Substring(24);
@@ -31,58 +35,12 @@ namespace CKS.Web.Controllers
 					search.ErrorMessage,
 					searchUrl.ToString(),
 					logId)));
+
+				statusCode = 500;
 			}
-			return search;
+
+			return new JsonResult(search) { StatusCode = statusCode };
 		}
 
-			//[HttpGet]
-			//public object Get()
-			//{
-			//	return new
-			//	{
-			//		Failed = false,
-			//		ResultCount = 26,
-			//		RolledUpCount = 26,
-			//		UnrolledCount = 26,
-			//		PageSize = 10,
-			//		FirstResult = 1,
-			//		LastResult = 10,
-			//		FinalSearchText = "acne",
-			//		FinalSearchTextNoStopWords = "acne",
-			//		PagerLinks = new
-			//		{
-			//			Pages = new[] {
-			//				new {
-			//					Title = "1",
-			//					url = new {
-			//						fullUrl = "search?pa=1&q=acne"
-			//					},
-			//					IsCurrent = true
-			//				},
-			//				new {
-			//					Title = "2",
-			//					url = new {
-			//						fullUrl = "search?pa=2&q=acne"
-			//					},
-			//					IsCurrent = false
-			//				}
-			//			}
-			//		},
-			//		Documents = new[] {
-			//			new {
-			//				Id = "651475",
-			//				PathAndQuery = "/acne-vulgaris",
-			//				Teaser = "Acne vulgaris is a chronic skin condition in which blockage or inflammation of the hair follicles and accompanying sebaceous glands",
-			//				Title = "<b>Acne</b> vulgaris",
-			//			},
-			//			new {
-			//				Id = "651781",
-			//				PathAndQuery = "/rosacea-acne",
-			//				Teaser = "Acne rosacea is a chronic relapsing skin condition affecting the face, characterized by episodes of facial flushing, erythema, telangiectasia.",
-			//				Title = "Rosacea - <b>acne</b>",
-			//			}
-			//		}
-			//	};
-			//}
-		}
+	}
 }
