6bcbeb5a81d78a47981f2bde835885f72c0de0a9
Ian Routledge <ian.routledge@nice.org.uk>
2020-05-22 13:26:34 +0100

    CKS-303 Add GTM container (#25)
    * CKS-303 Add GTM snippet
    
    * CKS-303 Update package lock after merge
    
    * CKS-303 Update gatsby version
    
    Because of this issue: https://github.com/gatsbyjs/gatsby/issues/23378
    
    * CKS-303 Web app serves from Gatsby public folder
    
    * CKS-303 Add feature test for tag manager
    
    * CKS-303 Add functional tests tasks to vscode
    
    * CKS-303 Fix spelling mistake
    
    * CKS-303 Update packages

--- web-app/CKS.Web/Startup.cs
+++ web-app/CKS.Web/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NICE.Search.Common.Interfaces;
@@ -17,18 +19,18 @@ using NICE.Search.Common.Enums;
 
 namespace CKS.Web
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
 			services.AddRouting(options => options.LowercaseUrls = true);
 
 			var environmentString = Configuration.GetValue<string>("ElasticSearchEnvironment");
@@ -40,27 +42,42 @@ namespace CKS.Web
 			}
 
 			services.AddControllers();
-        }
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+
+		public void ConfigureDevelopment(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			// Serve static files straight for Gatsby's public folder when running locally.
+			// This means you don't have to copy the Gatsby output into the wwwroot folder like we do on TeamCity.
+			var localGatsbyFileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "../../gatsby/public"));
+			app.UseDefaultFiles(new DefaultFilesOptions {
+					FileProvider = localGatsbyFileProvider
+				});
+			app.UseStaticFiles(new StaticFileOptions() {
+					FileProvider = localGatsbyFileProvider
+				});
+
+			Configure(app, env);
+		}
+	}
 }
