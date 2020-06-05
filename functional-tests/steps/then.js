d7e287de9f88f3bc41372740fda4b53921baf196
Ian Routledge <ian.routledge@nice.org.uk>
2020-06-05 16:38:00 +0100

    CKS-309 Fix 404 page functional test

--- functional-tests/steps/then.js
+++ functional-tests/steps/then.js
@@ -26,9 +26,7 @@ Then(/^I see "([^"]*)" in the autocomplete suggestions$/, (text) => {
 Then(
 	/^I should see a meta tag named "([^"]*)" with the content "([^"]*)"$/,
 	(name, content) => {
-		const selector = `meta[name='${name}]`;
-		expect(browser.element(selector).getAttribute("content")).to.equal(
-			content
-		);
+		const selector = `meta[name='${name}']`;
+		expect(browser.element(selector).getAttribute("content")).to.equal(content);
 	}
 );
