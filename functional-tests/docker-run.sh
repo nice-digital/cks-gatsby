a3154041ffc5944df48b3ff81ffc0e0578806d20
dylan <dylan.riley@nice.org.uk>
2020-06-10 16:12:57 +0100

    CKS-331 Testing docker-run.sh on TC

--- functional-tests/docker-run.sh
+++ functional-tests/docker-run.sh
@@ -2,29 +2,67 @@
 
 # Runs functional tests via Docker
 
-# Clean up before we start
-rm -rf docker-output && rm -rf allure-results && rm -rf allure-report
+function cleanupBeforeStart()
+{
+  # Clean up before we start
+  rm -rf docker-output && rm -rf allure-results && rm -rf allure-report
 
-# Avoid "Mount denied" errors for Chrome/Firefox containers on Windows
-# See https://github.com/docker/for-win/issues/1829#issuecomment-376328022
-export COMPOSE_CONVERT_WINDOWS_PATHS=1
+  # Avoid "Mount denied" errors for Chrome/Firefox containers on Windows
+  # See https://github.com/docker/for-win/issues/1829#issuecomment-376328022
+  export COMPOSE_CONVERT_WINDOWS_PATHS=1
 
-# Clean up before starting containers
-docker-compose down --remove-orphans --volumes && docker-compose rm -vf
+  # Clean up before starting containers
+  docker-compose down --remove-orphans --volumes && docker-compose rm -vf
+}
+
+
+
+function runTests()
+{
+  # Wait for the web app to be up before running the tests
+  docker-compose run -T test-runner npm run wait-then-test
+  errormessage=$?
+  echo $errorCode
+  if [ $errorCode -eq 1 ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function processTestOutput()
+{
+  # Generate an Allure test report
+  docker-compose run -T test-runner allure generate --clean
+
+  # Copy error shots and logs to use as a TeamCity artifact for debugging purposes
+  mkdir -p docker-output
+  docker cp test-runner:/tests/screenshots ./docker-output
+  docker cp test-runner:/tests/allure-report ./docker-output
+  #docker cp cks-web-app:/app/logs ./docker-output/cks-web-app
+  docker-compose logs --no-color > ./docker-output/logs.txt
+}
+
+
+
+function cleanupAtEnd()
+{
+  # Clean up
+  docker-compose down --remove-orphans --volumes
+  echo "$1"
+  if [ $1 -gt 0 ]
+  then
+    exit 1
+  else
+    exit 0
+  fi
+}
+
+cleanupBeforeStart
 docker-compose up -d
-
-# Wait for the web app to be up before running the tests
-docker-compose run -T test-runner npm run wait-then-test
-
-# Generate an Allure test report
-docker-compose run -T test-runner allure generate --clean
-
-# Copy error shots and logs to use as a TeamCity artifact for debugging purposes
-mkdir -p docker-output
-docker cp test-runner:/tests/screenshots ./docker-output
-docker cp test-runner:/tests/allure-report ./docker-output
-#docker cp cks-web-app:/app/logs ./docker-output/cks-web-app
-docker-compose logs --no-color > ./docker-output/logs.txt
-
-# Clean up
-docker-compose down --remove-orphans --volumes
+runTests
+error=$?
+echo "dylan $error"
+processTestOutput
+cleanupAtEnd error
