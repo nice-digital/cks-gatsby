a76cf83b82e0e997408859cab90e59d299452689
Warren Keith <w@rren.dev>
2020-06-18 17:37:37 +0100

    CKS-251 Multiple amends
    - Consolidated error handling for dev and production
    - Added pageheader component
    - Removed react-html-parser
    - Markup improvements

--- gatsby/src/pages/search.tsx
+++ gatsby/src/pages/search.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { PageRendererProps, Link } from "gatsby";
-import parser from "react-html-parser";
 import { Card } from "@nice-digital/nds-card";
 import { SimplePagination } from "@nice-digital/nds-simple-pagination";
 import { Alert } from "@nice-digital/nds-alert";
+import { PageHeader } from "@nice-digital/nds-page-header";
 
 import { Layout } from "../components/Layout/Layout";
 import { SEO } from "../components/SEO/SEO";
@@ -50,14 +50,10 @@ type Document = {
 	url: string;
 };
 
-function titleString(text: string, page: number): string {
-	return text
-		? page > 1
-			? `Page ${page} | ${text} | `
-			: `${text} | `
-		: page > 1
-		? `Page ${page} | `
-		: "";
+function titleString(searchText: string, pageIndex: number): string {
+	return [pageIndex > 1 && `Page ${pageIndex}`, searchText, "Search results"]
+		.filter(Boolean)
+		.join(" | ");
 }
 
 const SearchPage: React.FC<SearchPageProps> = ({
@@ -76,21 +72,12 @@ const SearchPage: React.FC<SearchPageProps> = ({
 
 	return (
 		<Layout>
-			<h1>Search</h1>
-			{!error && !data && <div>Loading</div>}
+			{!error && !data && (
+				<PageHeader heading="Search results" lead="loading" />
+			)}
+			{error?.message && <ErrorBlock error={error} />}
 			{data && data.failed && <div>Failed to load results</div>}
 			{data && !data.failed && <Results {...data} />}
-			{error?.message && (
-				<Alert type="error">
-					<pre>
-						{error.name}
-						<br />
-						{error.message}
-						<br />
-						{error.stack}
-					</pre>
-				</Alert>
-			)}
 		</Layout>
 	);
 };
@@ -107,26 +94,29 @@ const Results: React.FC<SearchResults> = ({
 	const currentPage = Math.ceil(firstResult / pageSize);
 	const totalPages = Math.ceil(resultCount / pageSize);
 	const paginationProps = {
-		nextPageLink: next?.fullUrl
-			? { destination: "/" + next?.fullUrl, elementType: Link }
-			: undefined,
-		previousPageLink: previous?.fullUrl
-			? { destination: "/" + previous?.fullUrl, elementType: Link }
-			: undefined,
+		nextPageLink:
+			(next?.fullUrl && {
+				destination: "/" + next?.fullUrl,
+				elementType: Link,
+			}) ||
+			undefined,
+		previousPageLink:
+			(previous?.fullUrl && {
+				destination: "/" + previous?.fullUrl,
+				elementType: Link,
+			}) ||
+			undefined,
 		currentPage,
 		totalPages,
 	};
 
 	return (
 		<>
-			<SEO
-				title={`${titleString(finalSearchText, currentPage)}Search results`}
-				noIndex={true}
-			/>
+			<SEO title={titleString(finalSearchText, currentPage)} noIndex={true} />
 			<ResultSummary
 				resultCount={resultCount}
 				finalSearchText={finalSearchText}
-				originalSearch={originalSearch?.searchText}
+				originalSearchText={originalSearch?.searchText}
 			/>
 			{documents.length > 0 && <ResultsList documents={documents} />}
 			{resultCount > pageSize && <SimplePagination {...paginationProps} />}
@@ -137,37 +127,56 @@ const Results: React.FC<SearchResults> = ({
 interface ResultsSummary {
 	resultCount: number;
 	finalSearchText: string;
-	originalSearch: string;
+	originalSearchText: string;
 }
 
 const ResultSummary: React.FC<ResultsSummary> = ({
 	resultCount,
 	finalSearchText,
-	originalSearch,
+	originalSearchText,
 }) => {
 	if (resultCount === 0) {
-		return <NoResults searchText={finalSearchText} />;
+		const searchTerm = originalSearchText
+			? originalSearchText
+			: finalSearchText;
+		return (
+			<>
+				<PageHeader
+					heading="No results found"
+					lead={
+						<span
+							dangerouslySetInnerHTML={{
+								__html: `We couldn&apos;t find any results for <b>${searchTerm}</b>
+								<br />
+								Check for spelling mistakes or try another search term.`,
+							}}
+						/>
+					}
+				/>
+				<NoResults searchText={searchTerm} />
+			</>
+		);
 	}
 
-	return (
+	const LeadText = (
 		<>
-			<p>
-				{originalSearch && (
-					<>
-						Your search for <b>{originalSearch}</b> returned no results
-						<br />
-					</>
-				)}
-				{resultCount} {resultCount === 1 ? "result" : "results"}
-				{finalSearchText && (
-					<>
-						{" "}
-						for <b>{finalSearchText}</b>
-					</>
-				)}
-			</p>
+			{originalSearchText && (
+				<>
+					Your search for <b>{originalSearchText}</b> returned no results
+					<br />
+				</>
+			)}
+			{resultCount} {resultCount === 1 ? "result" : "results"}
+			{finalSearchText && (
+				<>
+					{" "}
+					for <b>{finalSearchText}</b>
+				</>
+			)}
 		</>
 	);
+
+	return <PageHeader heading="Search results" lead={LeadText} />;
 };
 
 interface ResultsList {
@@ -175,17 +184,17 @@ interface ResultsList {
 }
 
 const ResultsList: React.FC<ResultsList> = ({ documents }) => (
-	<ul className="list--unstyled">
+	<ol className="list--unstyled">
 		{documents?.map(({ id, title, pathAndQuery, teaser }) => (
 			<li key={id}>
 				<Card
-					summary={parser(teaser)}
-					headingText={parser(title)}
-					link={{ destination: `/topics/${pathAndQuery}`, elementType: Link }}
+					summary={<span dangerouslySetInnerHTML={{ __html: teaser }} />}
+					headingText={<span dangerouslySetInnerHTML={{ __html: title }} />}
+					link={{ destination: `${pathAndQuery}`, elementType: Link }}
 				/>
 			</li>
 		))}
-	</ul>
+	</ol>
 );
 
 interface NoResultsProps {
@@ -196,83 +205,110 @@ const NoResults: React.FC<NoResultsProps> = ({
 	searchText,
 }: NoResultsProps) => (
 	<>
-		<h1>No results found</h1>
-		<p>
-			We couldn&apos;t find any results for <b>{searchText}</b>
-		</p>
-		<p>Check for spelling mistakes or try another search term.</p>
-		<h2>Browse for topics</h2>
-		<p>Try browsing for topics with our:</p>
-		<ul>
-			<li>
-				<Link to={"/topics"}>A-Z list</Link>
-			</li>
-			<li>
-				<Link to={"/specialities"}>Clinical specialities</Link>
-			</li>
-		</ul>
-		<h2>Check our other services</h2>
-		<p>
-			See if there are search results for <b>{searchText}</b> on our other
-			services:
-		</p>
-		<ul>
-			<li>
-				<a
-					rel="noreferrer"
-					target="_blank"
-					href={`https://bnf.nice.org.uk/#Search?q=${searchText}`}
-				>
-					BNF
-				</a>
-			</li>
-			<li>
-				<a
-					rel="noreferrer"
-					target="_blank"
-					href={`https://bnfc.nice.org.uk/#Search?q=${searchText}`}
-				>
-					BNFc
-				</a>
-			</li>
-			<li>
-				<a
-					rel="noreferrer"
-					target="_blank"
-					href={`https://www.evidence.nhs.uk/search?q=${searchText}`}
-				>
-					Evidence search
-				</a>
-			</li>
-			<li>
-				<a
-					rel="noreferrer"
-					target="_blank"
-					href={`https://www.nice.org.uk/search?om=[{%22ndt%22:[%22Guidance%22]}]&ps=15&q=${searchText}&sp=on`}
-				>
-					NICE guidance
-				</a>
-			</li>
-		</ul>
-		<h3>What are our other services?</h3>
-		<h4>BNF and BNFc</h4>
-		<p>
-			Drug and prescribing information for healthcare professionals. A reference
-			for correct dosage, indication, interaction and side effects of drugs.
-		</p>
-		<h4>Evidence search</h4>
-		<p>
-			A search engine for selected, authoritative evidence in health, social
-			care and public health. Brings together high-quality evidence from
-			hundreds of trusted sources. Includes guidance, systematic reviews,
-			evidence summaries, and patient information.
-		</p>
-		<h4>NICE guidance</h4>
-		<p>
-			We use the best available evidence to develop recommendations that guide
-			decisions in health, public health and social care.
-		</p>
+		<section>
+			<h3>Browse for topics</h3>
+			<p>Try browsing for topics with our:</p>
+			<ul>
+				<li>
+					<Link to={"/topics/"}>A-Z list</Link>
+				</li>
+				<li>
+					<Link to={"/specialities/"}>Clinical specialities</Link>
+				</li>
+			</ul>
+		</section>
+		<section>
+			<h3>Check our other services</h3>
+			<p>
+				See if there are search results for <b>{searchText}</b> on our other
+				services:
+			</p>
+			<ul aria-label={`Search for "${searchText}" on our other services`}>
+				<li>
+					<a
+						rel="noreferrer"
+						target="_blank"
+						href={`https://bnf.nice.org.uk/#Search?q=${searchText}`}
+					>
+						BNF
+					</a>
+				</li>
+				<li>
+					<a
+						rel="noreferrer"
+						target="_blank"
+						href={`https://bnfc.nice.org.uk/#Search?q=${searchText}`}
+					>
+						BNFc
+					</a>
+				</li>
+				<li>
+					<a
+						rel="noreferrer"
+						target="_blank"
+						href={`https://www.evidence.nhs.uk/search?q=${searchText}`}
+					>
+						Evidence search
+					</a>
+				</li>
+				<li>
+					<a
+						rel="noreferrer"
+						target="_blank"
+						href={`https://www.nice.org.uk/search?om=[{%22ndt%22:[%22Guidance%22]}]&ps=15&q=${searchText}&sp=on`}
+					>
+						NICE guidance
+					</a>
+				</li>
+			</ul>
+		</section>
+		<section>
+			<h4>What are our other services?</h4>
+			<h5>BNF and BNFc</h5>
+			<p>
+				Drug and prescribing information for healthcare professionals. A
+				reference for correct dosage, indication, interaction and side effects
+				of drugs.
+			</p>
+			<h5>Evidence search</h5>
+			<p>
+				A search engine for selected, authoritative evidence in health, social
+				care and public health. Brings together high-quality evidence from
+				hundreds of trusted sources. Includes guidance, systematic reviews,
+				evidence summaries, and patient information.
+			</p>
+			<h5>NICE guidance</h5>
+			<p>
+				We use the best available evidence to develop recommendations that guide
+				decisions in health, public health and social care.
+			</p>
+		</section>
 	</>
 );
+
+const ErrorBlock: React.FC<{ error: Error }> = ({
+	error: { name, message, stack },
+}) => {
+	if (process?.env?.GATSBY_ENV === "development")
+		return (
+			<Alert type="error">
+				<pre>
+					{name}
+					<br />
+					{message}
+					<br />
+					{stack}
+				</pre>
+			</Alert>
+		);
+	return (
+		<PageHeader
+			heading="Error"
+			lead="We are currently experiencing issues with search. The issue will be
+	resolved as soon as possible. We apologise for any inconvenience caused.
+"
+		/>
+	);
+};
 
 export default SearchPage;
