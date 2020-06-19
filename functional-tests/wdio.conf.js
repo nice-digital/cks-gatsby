9dcdacd7accb2436b94faae5281b3fba8a365664
Ian Routledge <ian.routledge@nice.org.uk>
2020-06-19 17:04:37 +0100

    CKS-353 Runs tests in parallal and headless

--- functional-tests/wdio.conf.js
+++ functional-tests/wdio.conf.js
@@ -5,7 +5,7 @@ const isInDocker = !!process.env.IN_DOCKER,
 
 exports.config = {
 	sync: true,
-	maxInstances: isInDocker ? 5 : 2,
+	maxInstances: isInDocker ? 10 : 2,
 	services: isInDocker ? [] : ["selenium-standalone"],
 	seleniumLogs: "./logs",
 
@@ -15,20 +15,16 @@ exports.config = {
 		{
 			browserName: "chrome",
 			chromeOptions: {
-				args: ["--window-size=1366,768"],
+				args: ["--window-size=1366,768"].concat(isInDocker ? "--headless" : []),
 			},
 		},
-		//Disabled firefox tests as there is a card open to fix them: CKS-301
-		// {
-		// 	browserName: "firefox",
-		// },
 	],
 
 	logLevel: "error",
 	coloredLogs: true,
 	screenshotPath: "./screenshots/",
 	baseUrl: "http://localhost:5000/",
-	reporters: isTeamCity ? ["spec", "teamcity", "allure"] : ["spec"],
+	reporters: isTeamCity ? ["spec", "teamcity"] : ["spec"],
 
 	// Use BDD with Cucumber
 	framework: "cucumber",
