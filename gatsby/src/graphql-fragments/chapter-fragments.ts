1a928ce62a96ccbadb44aacbe6d53074a746e03d
Ian Routledge <ian.routledge@nice.org.uk>
2020-06-22 10:46:37 +0100

    CKS-334 Remove unusde fragments

--- gatsby/src/graphql-fragments/chapter-fragments.ts
+++ gatsby/src/graphql-fragments/chapter-fragments.ts
@@ -5,47 +5,12 @@ export const PartialChapter = graphql`
 		id
 		slug
 		fullItemName
-		# We only need 2 levels of chapters for menus
+		# We only need 2 levels of chapters for the topic menu
+		# so no need to select subChapter recursively here
 		subChapters {
 			id
 			slug
 			fullItemName
-		}
-	}
-`;
-
-export const PartialChapterWithContent = graphql`
-	fragment PartialChapterWithContent on CksChapter {
-		...PartialChapter
-		htmlStringContent
-		htmlHeader
-	}
-`;
-
-export const FullChapter = graphql`
-	fragment FullChapter on CksChapter {
-		...PartialChapterWithContent
-		# Nest 'enough' levels deep for nested chapters because GraphQL doesn't allow recursion
-		subChapters {
-			...PartialChapterWithContent
-			subChapters {
-				...PartialChapterWithContent
-				subChapters {
-					...PartialChapterWithContent
-					subChapters {
-						...PartialChapterWithContent
-					}
-				}
-			}
-		}
-		parentChapter {
-			...PartialChapter
-		}
-		topic {
-			...PartialTopic
-			chapters {
-				...PartialChapter
-			}
 		}
 	}
 `;
