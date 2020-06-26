9d4c99194ca623014893f906f7b3ff2699a28568
Ian Routledge <ian.routledge@nice.org.uk>
2020-06-26 09:31:38 +0100

    CKS-243 Refactor tests and add about page tests

--- functional-tests/steps/when.js
+++ functional-tests/steps/when.js
@@ -6,21 +6,32 @@ import waitFor from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitFo
 import debug from "@nice-digital/wdio-cucumber-steps/lib/support/action/debug";
 
 import typeInSearchBox from "../support/action/typeInSearchBox";
+import { getSelector } from "../support/selectors";
 
 When(/^I type "([^"]*)" in the header search box$/, typeInSearchBox);
 
 When(/^I click "([^"]*)" in the autocomplete options$/, (text) => {
 	typeInSearchBox("Ast");
-	waitFor(".autocomplete__option");
+	waitFor(getSelector("autocomplete option"));
 
 	// For some reason we can't click on an autocomplete suggestion via wdio's
 	// browser.click(element). So we have to use this workaround:
-	browser.execute((text) => {
-		document.querySelectorAll(".autocomplete__option a").forEach((element) => {
-			if (element.textContent.toLowerCase().indexOf(text.toLowerCase()) > -1) {
-				element.click();
-				return;
-			}
-		});
-	}, text);
+	browser.execute(
+		(text, optionAnchorSelector) => {
+			document.querySelectorAll(optionAnchorSelector).forEach((element) => {
+				if (
+					element.textContent.toLowerCase().indexOf(text.toLowerCase()) > -1
+				) {
+					element.click();
+					return;
+				}
+			});
+		},
+		text,
+		getSelector("autocomplete anchor")
+	);
+});
+
+When(/^I click on the "([^"]*)" breadcrumb$/, (breadcrumbText) => {
+	$(getSelector("breadcrumbs list")).$(`=${breadcrumbText}`).click();
 });
