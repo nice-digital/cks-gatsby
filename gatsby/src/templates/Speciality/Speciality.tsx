762be34be73a99e1984b882e02f4ef14e0d6b8f0
Ian Routledge <ian.routledge@nice.org.uk>
2020-06-27 22:24:07 +0100

    CKS-242 Add specialities page styles

--- gatsby/src/templates/Speciality/Speciality.tsx
+++ gatsby/src/templates/Speciality/Speciality.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { graphql, PageRendererProps, Link } from "gatsby";
+import { graphql, PageProps, Link } from "gatsby";
+
+import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { PageHeader } from "@nice-digital/nds-page-header";
 
 import { Layout } from "../../components/Layout/Layout";
 import { Speciality } from "../../types";
 import { SEO } from "../../components/SEO/SEO";
+import { ColumnList } from "../../components/ColumnList/ColumnList";
 
-type SpecialityPageProps = {
-	data: {
+type SpecialityPageProps = PageProps<
+	{
 		speciality: Speciality;
-	};
-} & PageRendererProps;
+	},
+	{ id: string }
+>;
 
 const SpecialityPage: React.FC<SpecialityPageProps> = ({
 	data: {
@@ -19,15 +24,27 @@ const SpecialityPage: React.FC<SpecialityPageProps> = ({
 	return (
 		<Layout>
 			<SEO title={name + " | Specialities"} />
-			<h1>Speciality: {name}</h1>
-			Topics:
-			<ul>
+
+			<Breadcrumbs>
+				<Breadcrumb to="https://www.nice.org.uk/">NICE</Breadcrumb>
+				<Breadcrumb to="/" elementType={Link}>
+					CKS
+				</Breadcrumb>
+				<Breadcrumb to="/specialities/" elementType={Link}>
+					Specialities
+				</Breadcrumb>
+				<Breadcrumb>{name}</Breadcrumb>
+			</Breadcrumbs>
+
+			<PageHeader heading={name} />
+
+			<ColumnList aria-label={`A to Z of topics within ${name}`}>
 				{topics.map(({ id, topicName, slug }) => (
 					<li key={id}>
 						<Link to={`/topics/${slug}/`}>{topicName}</Link>
 					</li>
 				))}
-			</ul>
+			</ColumnList>
 		</Layout>
 	);
 };
