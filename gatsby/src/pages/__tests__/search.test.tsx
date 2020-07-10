e593bf67b8e2a4b8c0d88a570eb1e92065a4d4b5
Ian Routledge <ian.routledge@nice.org.uk>
2020-07-10 20:50:08 +0100

    CKS-282 Refactor waitForDomChange
    To waitFor, because of deprecation

--- gatsby/src/pages/__tests__/search.test.tsx
+++ gatsby/src/pages/__tests__/search.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import fetch from "jest-fetch-mock";
 import { renderWithRouter, textContentMatcher } from "test-utils";
-import { waitForDomChange, waitFor, fireEvent } from "@testing-library/react";
+import { waitFor, fireEvent } from "@testing-library/react";
 import * as searchResponseLong from "./sample-data/search-response-long.json";
 import * as searchResponseLongPage2 from "./sample-data/search-response-long-page-2.json";
 import * as searchResponseShort from "./sample-data/search-response-short.json";
@@ -17,62 +17,72 @@ describe("Search Page", () => {
 	it("should render a loading message before the request comes in", async () => {
 		const { queryByText } = renderWithRouter(<SearchPage />);
 		expect(queryByText("Loading")).toBeInTheDocument();
-		await waitForDomChange();
-		expect(queryByText("Loading")).not.toBeInTheDocument();
+		await waitFor(() => {
+			expect(queryByText("Loading")).not.toBeInTheDocument();
+		});
 	});
 
 	it("should display the total number of search results", async () => {
 		fetch.mockResponse(JSON.stringify(searchResponseLong));
 		const component = renderWithRouter(<SearchPage />);
-		await waitForDomChange();
-		expect(
-			component.queryByText(
-				textContentMatcher(
-					`${searchResponseLong.resultCount.toString()} results for ${
-						searchResponseLong.finalSearchText
-					}`
+		await waitFor(() => {
+			expect(
+				component.queryByText(
+					textContentMatcher(
+						`${searchResponseLong.resultCount.toString()} results for ${
+							searchResponseLong.finalSearchText
+						}`
+					)
 				)
-			)
-		).toBeInTheDocument();
+			).toBeInTheDocument();
+		});
 	});
 
 	describe("Breadcrumbs", () => {
 		it("should not link to the original query if we're on page 1", async () => {
 			fetch.mockResponse(JSON.stringify(searchResponseLong));
 			const component = renderWithRouter(<SearchPage />);
-			await waitForDomChange();
-			expect(
-				component.queryByText(textContentMatcher("Search results for test"), {
-					selector: ".breadcrumbs a",
-				})
-			).toBeFalsy();
+			await waitFor(() => {
+				expect(
+					component.queryByText(textContentMatcher("Search results for test"), {
+						selector: ".breadcrumbs a",
+					})
+				).toBeFalsy();
+			});
 		});
+
 		it("should include a link to the original query if we're on page >= 2", async () => {
 			fetch.mockResponse(JSON.stringify(searchResponseLongPage2));
 			const component = renderWithRouter(<SearchPage />);
-			await waitForDomChange();
-			expect(
-				component.queryByText(textContentMatcher("Search results for test"), {
-					selector: ".breadcrumbs a",
-				})
-			).toBeTruthy();
+			await waitFor(() => {
+				expect(
+					component.queryByText(textContentMatcher("Search results for test"), {
+						selector: ".breadcrumbs a",
+					})
+				).toBeTruthy();
+			});
 		});
 	});
 
 	it("should not show any pagination if there are fewer results than the supplied page limit", async () => {
 		fetch.mockResponse(JSON.stringify(searchResponseShort));
 		const component = renderWithRouter(<SearchPage />);
-		await waitForDomChange();
-		expect(component.container.querySelector(".simple-pagination")).toBeFalsy();
+		await waitFor(() => {
+			expect(
+				component.container.querySelector(".simple-pagination")
+			).toBeFalsy();
+		});
 	});
 
 	it("should show both the original search term and the corrected search term if present", async () => {
 		fetch.mockResponse(JSON.stringify(searchResponseShort));
 		const component = renderWithRouter(<SearchPage />);
-		await waitForDomChange();
-		expect(
-			await component.findByText(searchResponseShort.originalSearch.searchText)
-		).toBeInTheDocument();
+		await waitFor(() => {
+			expect(
+				component.getByText(searchResponseShort.originalSearch.searchText)
+			).toBeInTheDocument();
+		});
+
 		expect(
 			await component.findByText(searchResponseShort.finalSearchText)
 		).toBeInTheDocument();
@@ -81,12 +91,13 @@ describe("Search Page", () => {
 	it("should fall back to error message if the response doesn't come back", async () => {
 		fetch.mockReject(new Error("Something's gone wrong!"));
 		const component = renderWithRouter(<SearchPage />);
-		await waitForDomChange();
-		expect(
-			component.queryByText(textContentMatcher("Error"), {
-				selector: "h1",
-			})
-		).toBeInTheDocument();
+		await waitFor(() => {
+			expect(
+				component.queryByText(textContentMatcher("Error"), {
+					selector: "h1",
+				})
+			).toBeInTheDocument();
+		});
 	});
 
 	describe("Screen reader announcements", () => {
