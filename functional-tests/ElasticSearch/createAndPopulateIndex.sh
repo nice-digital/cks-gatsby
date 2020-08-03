effec3d1e221645fd5372d0d8588b13d48deed17
dylan <dylan.riley@nice.org.uk>
2020-08-03 19:34:32 +0100

    CKS-302 Populate es index with load upload

--- functional-tests/ElasticSearch/createAndPopulateIndex.sh
+++ functional-tests/ElasticSearch/createAndPopulateIndex.sh
@@ -5,38 +5,9 @@ do
    sleep 5s
 done
 
-
-#create indexes
-#Naming the index cks means we done need to create an alias
+#create index
 curl -X PUT http://elasticsearch:9200/cks -H 'Content-Type: application/json' --data-binary "@./elasticSetup/CKSIndexSettings.json"
 
-#populate indexes
-arr=(./elasticSetup/topics/*)
+#Put data into the index
+curl -X PUT http://elasticsearch:9200/cks/document/_bulk -H 'Content-Type: application/json' --data-binary "@./elasticSetup/esIndexData.json"
 
-# iterate through array using a counter
-for ((i=0; i<${#arr[@]}; i++));
-do
-	#Get topicname prop from json in the current file
-	topicName=`grep -i 'topicName' ${arr[$i]}`
-	#Change topicname to title. This is specified in the elasticsearch document definition
-	topicName="${topicName/"topicName"/"title"}"
-	
-	#Get topicSummary prop from json in the current file
-	topicSummary=`grep -i -m1 'topicSummary' ${arr[$i]}`
-	#For testing we use the topicSummary as the content.
-	topicSummary="${topicSummary/"topicSummary"/"content"}"
-	
-	#remove trailing comma
-	#topicSummary=${topicSummary::-1}
-	
-	#Boostvalue field is required as specified in the elasticsearch document definition
-	#Omission will cause any searches to fail
-	boostValue='"boostvalue": 100'
-	
-	json="{$topicName $topicSummary $boostValue}"
-	url="http://elasticsearch:9200/cks/document/$i"
-	
-	curl -X PUT $url -H 'Content-Type: application/json' -d "$json"
-	echo $url
-	echo $json
-done
---
+++ functional-tests/ElasticSearch/createEsIndexData.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#populate indexes
+arr=(../../fake-api/data/topics/*)
+
+# iterate through array using a counter
+for ((i=0; i<${#arr[@]}; i++));
+do
+	#Get topicname prop from json in the current file
+	topicName=`grep -i 'topicName' ${arr[$i]}`
+	#Change topicname to title. This is specified in the elasticsearch document definition
+	topicName="${topicName/"topicName"/"title"}"
+	
+	#Get topicSummary prop from json in the current file
+	topicSummary=`grep -i -m1 'topicSummary' ${arr[$i]}`
+	#For testing we use the topicSummary as the content.
+	topicSummary="${topicSummary/"topicSummary"/"content"}"
+	
+	#remove trailing comma
+	#topicSummary=${topicSummary::-1}
+	
+	#Boostvalue field is required as specified in the elasticsearch document definition
+	#Omission will cause any searches to fail
+	boostValue='"boostvalue": 100'
+	
+	echo '{ "index": {} }' >> esIndexData.json
+	echo "{$topicName $topicSummary $boostValue}" >> esIndexData.json
+	
+done
