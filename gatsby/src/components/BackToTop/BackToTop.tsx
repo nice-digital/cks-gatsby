30620920d8e17e38f408c0df0553579bf55208ea
w@rren <w@rren.dev>
2020-08-06 12:34:17 +0100

    CKS-377 Add initial component

--- gatsby/src/components/BackToTop/BackToTop.tsx
+++ gatsby/src/components/BackToTop/BackToTop.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./BackToTop.module.scss";
 
@@ -8,10 +8,35 @@ interface BackToTopProps {
 
 export const BackToTop: React.FC<BackToTopProps> = ({
 	scrollTo = "content-start",
-}: BackToTopProps) => (
-	<div className={styles.wrapper}>
-		<div className="container">
-			<a href={`#${scrollTo}`}>Back to top (going to scroll to #{scrollTo})</a>
+}: BackToTopProps) => {
+	const [isFixed, setIsFixed] = useState(false);
+
+	function handleScroll() {
+		const footer = document.querySelector("footer");
+		if (!footer) return;
+		const footerVisible =
+			window.scrollY + window.innerHeight >
+			document.body.scrollHeight - footer.clientHeight;
+		const scrolledDown = window.scrollY > 800;
+		setIsFixed(!footerVisible && scrolledDown);
+	}
+
+	useEffect(() => {
+		document.addEventListener("scroll", handleScroll);
+		return () => {
+			document.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	return (
+		<div
+			className={[styles.backToTop, isFixed === true && styles.fixed].join(" ")}
+		>
+			<div className="container">
+				<a href={`#${scrollTo}`}>
+					Back to top (going to scroll to #{scrollTo})
+				</a>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
