a7b068a249509752a0e94c7e24393ef8222e31a3
Ian Routledge <ian@ediblecode.com>
2020-08-07 12:19:39 +0100

    CKS-404 Fix broken links

---
+++ gatsby/plugins/gatsby-source-cks/src/link-rewriter/node-model-extensions.ts
@@ -0,0 +1,53 @@
+import { NodeInput } from "gatsby";
+
+import { NodeModel } from "./types";
+import { TopicNode, topicNodeType } from "../node-creation/topics";
+import { ChapterNode, chapterNodeType } from "../node-creation/chapters";
+
+const TopicIdField = "topicId";
+const ChapterIdField = "itemId";
+
+interface GetNodeByIdArgs {
+	idField: string;
+	id: string;
+	nodeTypeName: string;
+	nodeModel: NodeModel;
+}
+
+const getNodeById = <T extends NodeInput>({
+	idField,
+	id,
+	nodeTypeName,
+	nodeModel,
+}: GetNodeByIdArgs): Promise<T | null> =>
+	nodeModel.runQuery({
+		query: {
+			filter: {
+				[idField]: { eq: id },
+			},
+		},
+		firstOnly: true,
+		type: nodeTypeName,
+	});
+
+export const getTopicById = (
+	topicId: string,
+	nodeModel: NodeModel
+): Promise<TopicNode | null> =>
+	getNodeById({
+		idField: TopicIdField,
+		id: topicId,
+		nodeTypeName: topicNodeType,
+		nodeModel,
+	});
+
+export const getChapterById = (
+	chapterItemId: string,
+	nodeModel: NodeModel
+): Promise<ChapterNode | null> =>
+	getNodeById({
+		idField: ChapterIdField,
+		id: chapterItemId,
+		nodeTypeName: chapterNodeType,
+		nodeModel,
+	});
