3cc67968016d3b8ad9bd0dfee92ed7add36a543a
Ian Routledge <ian.routledge@nice.org.uk>
2020-11-18 18:10:48 +0000

    CKS-473 Revert async google fonts

--- gatsby/src/components/Layout/Layout.tsx
+++ gatsby/src/components/Layout/Layout.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode } from "react";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import { BackToTop } from "../BackToTop/BackToTop";
-import { Lato } from "../Lato/Lato";
 
 import "./Layout.scss";
 import styles from "../BackToTop/BackToTop.module.scss";
@@ -16,8 +15,16 @@ type LayoutProps = {
 export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
 	return (
 		<>
-			<Lato />
 			<Helmet>
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="crossOrigin"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400&display=swap"
+				/>
 				<script
 					src="https://cdn.nice.org.uk/cookie-banner/cookie-banner.min.js"
 					type="text/javascript"
