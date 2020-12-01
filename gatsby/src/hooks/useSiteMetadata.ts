f3b80fe15e56af448922d191c5e89f07d7bcd1fb
Ian Routledge <ian.routledge@nice.org.uk>
2020-12-01 12:46:10 +0000

    CKS-409 Add canonical and og:url tags (#120)
    * CKS-409 Add canonical and og:url link tags
    
    * CKS-409 Add canonical and og:url tags
    
    * CKS-409 Fix tests
    
    * CKS-409 Fix tests

---
+++ gatsby/src/hooks/useSiteMetadata.ts
@@ -0,0 +1,34 @@
+import { useStaticQuery, graphql } from "gatsby";
+
+interface SiteMetaData {
+	siteUrl: string;
+	title: string;
+	changesSinceDateISO: string;
+	changesSinceDateFormatted: string;
+}
+
+interface Site {
+	site: {
+		siteMetadata: SiteMetaData;
+	};
+}
+
+export const useSiteMetadata = (): SiteMetaData => {
+	const { site } = useStaticQuery<Site>(
+		graphql`
+			query SiteMetaData {
+				site {
+					siteMetadata {
+						title
+						siteUrl
+						changesSinceDateISO: changesSinceDate(formatString: "YYYY-MM")
+						changesSinceDateFormatted: changesSinceDate(
+							formatString: "MMMM YYYY"
+						)
+					}
+				}
+			}
+		`
+	);
+	return site.siteMetadata;
+};
