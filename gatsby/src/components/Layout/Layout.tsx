d8787ad0798e5917552e28f23722694dbae00965
Ian Routledge <ian.routledge@nice.org.uk>
2020-12-02 09:43:40 +0000

    CKS-485 Make render blocking resources async (#119)

--- gatsby/src/components/Layout/Layout.tsx
+++ gatsby/src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import React, { ReactNode } from "react";
+import { Helmet } from "react-helmet";
 
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import { BackToTop } from "../BackToTop/BackToTop";
+import { Lato } from "../Lato/Lato";
 
 import "./Layout.scss";
 import styles from "../BackToTop/BackToTop.module.scss";
-import { Helmet } from "react-helmet";
 
 type LayoutProps = {
 	children: ReactNode;
@@ -15,19 +16,12 @@ type LayoutProps = {
 export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
 	return (
 		<>
+			<Lato />
 			<Helmet>
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="crossOrigin"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400&display=swap"
-				/>
 				<script
 					src="https://cdn.nice.org.uk/cookie-banner/cookie-banner.min.js"
 					type="text/javascript"
+					async
 				></script>
 			</Helmet>
 			<Header />
