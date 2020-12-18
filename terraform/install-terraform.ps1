cdef4c1991daf0c9758209a5e3b8640a8d94bc1e
colinmazu <12713935+colinmazu@users.noreply.github.com>
2020-12-18 15:32:52 +0000

    CKS-488 Fixed issues with ps version

--- terraform/install-terraform.ps1
+++ terraform/install-terraform.ps1
@@ -2,46 +2,44 @@ Write-Host 'Checking PowerShell version.....'
 $PSVersionTable.PSVersion
 
 Write-Host "Starting Terraform Install"
-function Install-Terraform {
-    [CmdletBinding()]
-    param (
-        [String]$tfVersion = '0.14',
-        $InstallPath = '.',
-        $os = 'windows',
-        $arch = 'amd64'
-    )
 
-    if ($ENV:TF_VERSION) {$tfVersion = $ENV:TF_VERSION}
-    $tfAvailableVersions = (Invoke-Restmethod -UseBasicParsing 'https://releases.hashicorp.com/terraform/index.json').versions
+$tfVersion = "0.14"
+$InstallPath = '.'
+$os = 'windows'
+$arch = 'amd64'
 
-    #If a minor release was provided (e.g. 12.0), find the latest bugfix version available
-    if ($tfVersion -match '^\d+\.\d+$') {
-        $tfSearchVersion = [version]$tfVersion
-        #Exclude Prereleases
-        $tfAvailableVersionNums = [version[]]($tfAvailableVersions.psobject.properties.name | Where-Object {$PSItem -match '^\d+\.\d+\.\d+$'})
-        [string]$tfVersion = $tfAvailableVersionNums | where minor -eq $tfSearchVersion.Minor | Sort-Object -descending | Select-Object -first 1
-    }
 
-    $tfSelectedVersion = $tfAvailableVersions.$tfVersion
-    if (-not $TFSelectedVersion) {write-error "TF Version $TFVersion was not found on the terraform server"; return}
-    $tfDownloadUri = $tfSelectedVersion.builds | where os -eq $os | where arch -eq $arch | Select-Object -first 1 | % url
-    ("Selected Version: " + $tfSelectedVersion.version)
-    "Downloading $tfDownloadUri"
-    $DownloadPath= [io.path]::GetTempFileName()
-    #TODO: Hash Verification
-    if ($PSEdition -eq 'Desktop') {
-        #Fixes performance problem with Invoke-WebRequest
-        $ProgressPreference = 'Continue'
-    }
+if ($ENV:TF_VERSION) {$tfVersion = $ENV:TF_VERSION}
+$tfAvailableVersions = (Invoke-Restmethod -UseBasicParsing 'https://releases.hashicorp.com/terraform/index.json').versions
 
-    iwr -useb $tfDownloadUri -OutFile $DownloadPath
-    $ResolvedInstallPath = resolve-path $InstallPath
-    "Extracting terraform to $ResolvedInstallPath"
-    $tfZipPath = Join-Path (Split-Path $DownloadPath -Parent) 'terraform.zip'
-    Move-Item $DownloadPath $tfZipPath
-    Expand-Archive -Path $tfZipPath -DestinationPath $InstallPath -Force
-    if (($ResolvedInstallPath) -notin $env:PATH.split([IO.Path]::PathSeparator)) {write-warning "$ResolvedInstallPath was not found in your PATH environment variable. Call terraform directly with $(join-path $ResolvedInstallPath 'terraform')"}
+#If a minor release was provided (e.g. 12.0), find the latest bugfix version available
+if ($tfVersion -match '^\d+\.\d+$') {
+	$tfSearchVersion = [version]$tfVersion
+	#Exclude Prereleases
+	$tfAvailableVersionNums = [version[]]($tfAvailableVersions.psobject.properties.name | Where-Object {$PSItem -match '^\d+\.\d+\.\d+$'})
+	[string]$tfVersion = $tfAvailableVersionNums | where minor -eq $tfSearchVersion.Minor | Sort-Object -descending | Select-Object -first 1
 }
+
+$tfSelectedVersion = $tfAvailableVersions.$tfVersion
+if (-not $TFSelectedVersion) {write-error "TF Version $TFVersion was not found on the terraform server"; return}
+$tfDownloadUri = $tfSelectedVersion.builds | where os -eq $os | where arch -eq $arch | Select-Object -first 1 | % url
+("Selected Version: " + $tfSelectedVersion.version)
+"Downloading $tfDownloadUri"
+$DownloadPath= [io.path]::GetTempFileName()
+#TODO: Hash Verification
+if ($PSEdition -eq 'Desktop') {
+	#Fixes performance problem with Invoke-WebRequest
+	$ProgressPreference = 'Continue'
+}
+
+iwr -useb $tfDownloadUri -OutFile $DownloadPath
+$ResolvedInstallPath = resolve-path $InstallPath
+"Extracting terraform to $ResolvedInstallPath"
+$tfZipPath = Join-Path (Split-Path $DownloadPath -Parent) 'terraform.zip'
+Move-Item $DownloadPath $tfZipPath
+Expand-Archive -Path $tfZipPath -DestinationPath $InstallPath -Force
+if (($ResolvedInstallPath) -notin $env:PATH.split([IO.Path]::PathSeparator)) {write-warning "$ResolvedInstallPath was not found in your PATH environment variable. Call terraform directly with $(join-path $ResolvedInstallPath 'terraform')"}
+
 
 #Run the last function in this file unless this script is dotsourced, using any arguments provided
 # if ($myinvocation.Line -notmatch '^\. ') {
@@ -52,8 +50,6 @@ function Install-Terraform {
 #     [ScriptBlock]$invokeScriptBlock = [Scriptblock]::Create("$command $myargs")
 #     invoke-command $invokeScriptBlock
 # }
-
-Install-Terraform
 
 Write-Host "Deploying with Terraform"
 terraform --version
