5e35137e7b83eed447e9b30a2156728975ef17fc
Ian Routledge <ian.routledge@nice.org.uk>
2020-12-21 11:05:04 +0000

    CKS-343 Add on this page tests

--- gatsby/src/components/ChapterOnThisPage/ChapterOnThisPage.test.tsx
+++ gatsby/src/components/ChapterOnThisPage/ChapterOnThisPage.test.tsx
@@ -1,94 +1,127 @@
 import React from "react";
-import { render, RenderResult } from "@testing-library/react";
+import { fireEvent, render, RenderResult } from "@testing-library/react";
 
 import { ChapterOnThisPage } from "./ChapterOnThisPage";
-import { PartialChapter } from "src/types";
+import subChapers from "./subChapters.json";
+
+const querySelectorMockImpl = (slug: string, yPos: number) => (
+	selector: string
+) =>
+	({
+		getBoundingClientRect: () => ({
+			// Use a negative value for the dose heading to mock that we've scrolled to that heading
+			top: selector === `#${slug}` ? yPos : 101,
+		}),
+		getAttribute: (attrName: string) => (attrName === "id" ? slug : null),
+	} as Element);
 
 describe("ChapterOnThisPage", () => {
-	const subChapers: PartialChapter[] = [
-		{
-			id: "1-dose",
-			slug: "dose",
-			fullItemName: "Dose",
-			subChapters: [
-				{
-					id: "2-theophylline",
-					slug: "theophylline",
-					fullItemName: "Theophylline",
-					subChapters: [
-						{
-							id: "3-something",
-							slug: "something",
-							fullItemName: "Something",
-							subChapters: [],
-						},
-					],
-				},
-			],
-		},
-		{
-			id: "1-contraindications",
-			slug: "contraindications-cautions",
-			fullItemName: "Contraindications and cautions",
-			subChapters: [
-				{
-					id: "2-basis",
-					slug: "basis",
-					fullItemName: "Basis for recommendation",
-					subChapters: [],
-				},
-			],
-		},
-	];
+	let renderResult: RenderResult,
+		getByText: RenderResult["getByText"],
+		queryByText: RenderResult["queryByText"],
+		getByLabelText: RenderResult["getByLabelText"];
 
-	let renderResult: RenderResult;
 	beforeEach(() => {
 		renderResult = render(<ChapterOnThisPage subChapters={subChapers} />);
+		({ getByText, queryByText, getByLabelText } = renderResult);
+	});
+
+	afterEach(() => {
+		jest.restoreAllMocks();
 	});
 
 	it("should render 'on this page' heading 2", () => {
-		const { getByText } = renderResult;
 		expect(getByText("On this page").tagName).toBe("H2");
 	});
 
 	it("should render nav labelled by the heading", () => {
-		const { getByLabelText } = renderResult;
 		expect(getByLabelText("On this page").tagName).toBe("NAV");
 	});
 
 	it("should render ordered list of links", () => {
-		const { getByLabelText } = renderResult;
 		expect(getByLabelText("Jump links to section on this page").tagName).toBe(
 			"OL"
 		);
 	});
 
 	it("should render top 2 levels of chapters", () => {
-		const { getByText } = renderResult;
 		expect(getByText("Dose")).toBeInTheDocument();
 		expect(getByText("Contraindications and cautions")).toBeInTheDocument();
 		expect(getByText("Theophylline")).toBeInTheDocument();
 	});
 
 	it("should not render third level of sub chapter", () => {
-		const { queryByText } = renderResult;
 		expect(queryByText("Something")).toBeNull();
 	});
 
 	it("should should add specific class attribute to basis for recs sub chapters", () => {
-		const { getByText } = renderResult;
 		expect(getByText("Basis for recommendation").parentElement).toHaveClass(
 			"basisForRecs"
 		);
 	});
 
-	it.todo("should set aria current attribute for current scolled heading");
-	// fireEvent.scroll
+	it("should set current location aria attribute for current scolled heading", () => {
+		jest
+			.spyOn(document, "querySelector")
+			.mockImplementation(querySelectorMockImpl("dose", -10));
 
-	it.todo("should highlight active heading with a scroll threshold");
+		fireEvent.scroll(window);
 
-	it.todo("should add expanded class to root list item with active child");
+		expect(getByText("Dose")).toHaveAttribute("aria-current", "location");
+	});
 
-	it.todo("should detach window scroll event on unmount");
-	// unmount()
+	it("should highlight active heading with a scroll threshold", () => {
+		jest
+			.spyOn(document, "querySelector")
+			.mockImplementation(querySelectorMockImpl("dose", 99));
+
+		fireEvent.scroll(window);
+
+		expect(getByText("Dose")).toHaveAttribute("aria-current", "location");
+	});
+
+	it("should add expanded class to root list for current active heading", () => {
+		jest
+			.spyOn(document, "querySelector")
+			.mockImplementation(querySelectorMockImpl("dose", 99));
+
+		expect(getByLabelText("Sections within Dose")).not.toHaveClass(
+			"expandedSubList"
+		);
+
+		fireEvent.scroll(window);
+
+		expect(getByLabelText("Sections within Dose")).toHaveClass(
+			"expandedSubList"
+		);
+	});
+
+	it("should add expanded class to root list item with active child", () => {
+		jest
+			.spyOn(document, "querySelector")
+			.mockImplementation(querySelectorMockImpl("theophylline", 99));
+
+		expect(getByLabelText("Sections within Dose")).not.toHaveClass(
+			"expandedSubList"
+		);
+
+		fireEvent.scroll(window);
+
+		expect(getByLabelText("Sections within Dose")).toHaveClass(
+			"expandedSubList"
+		);
+	});
+
+	it("should detach window scroll event listener on unmount", () => {
+		const removeEventListenerSpy = jest.spyOn(window, "removeEventListener");
+
+		expect(removeEventListenerSpy).not.toHaveBeenCalled();
+
+		renderResult.unmount();
+
+		expect(removeEventListenerSpy).toHaveBeenCalledWith(
+			"scroll",
+			expect.any(Function)
+		);
+	});
 });
