cac8b9e885cc0393bb7a7aa652f4235eded91ce4
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-05 17:26:09 +0000

    CKS-508 Add updated tf files

---
+++ terraform/lambda.tf
@@ -0,0 +1,32 @@
+module "lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 1.0"
+
+  function_name = "CKS-Search-Lambda"
+  description   = "CKS Search Lambda API"
+  handler       = "CKS.SearchLambda::CKS.SearchLambda.Function::FunctionHandler"
+  runtime       = "dotnetcore3.1"
+  publish       = true
+
+  source_path = "../search-lambda/CKS.SearchLambda/bin/Release/netcoreapp3.1/linux-x64"
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service    = "apigateway"
+      source_arn = "${module.api_gateway.this_apigatewayv2_api_execution_arn}/*/*"
+    }
+  }
+}
+
+module "lambda_alias" {
+  source  = "terraform-aws-modules/lambda/aws//modules/alias"
+  version = "~> 1.0"
+
+  name = "prod"
+
+  function_name = module.lambda.this_lambda_function_name
+
+  # Set function_version when creating alias to be able to deploy using it,
+  # because AWS CodeDeploy doesn't understand $LATEST as CurrentVersion.
+  function_version = module.lambda.this_lambda_function_version
+}
