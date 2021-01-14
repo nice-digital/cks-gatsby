8bdbbbee0d5ef0d6807793a8585a875421f4f923
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-14 16:04:36 +0000

    CKS-499 Add zip

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -4,6 +4,8 @@ cat /etc/*-release
 echo "check for sudo...."
 sudo -l
 
+sudo apt-get install zip unzip
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
