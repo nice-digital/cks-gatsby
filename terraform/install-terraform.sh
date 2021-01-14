05f1ed15bc36a3ee550547067f1c2fc78176103d
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-14 15:42:58 +0000

    CKS-499 Check for sudo

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cat /etc/*-release
 
-wget "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -b ~/bin/aws
+echo "check for sudo...."
+sudo -l
+
+# wget "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+# unzip awscli-bundle.zip
+# ./awscli-bundle/install -b ~/bin/aws
 
 aws --version
 
