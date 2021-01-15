3ff47e96ae0d5d99fd449e9d4ddabb5e3964f77d
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-15 14:13:28 +0000

    CKS-499 Fix issue with unzip

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -7,7 +7,7 @@ sudo -l
 sudo apt-get install zip unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip -q
+unzip -q awscliv2.zip
 sudo ./aws/install --update
 
 aws --version
