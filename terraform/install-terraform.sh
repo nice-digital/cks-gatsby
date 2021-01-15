2cfd1f14341b0cb0ab7ef8a3cc9cc232a9cd24bd
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-15 16:31:28 +0000

    CKS-499 Install tf

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 cat /etc/*-release
 
-echo "check for sudo...."
-sudo -l
+echo "install useful tools...."
 
+sudo apt-get update
 sudo apt-get install zip unzip
+
+echo "install aws cli...."
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
@@ -13,17 +15,14 @@ sudo ./aws/install --update
 aws --version
 
 echo "set aws cli access keys...."
-echo $TFAWSAccessKey
 export AWS_ACCESS_KEY_ID=$(get_octopusvariable "TFAWSAccessKey")
 export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "TFAWSAccessSecret")
 export AWS_DEFAULT_REGION=eu-west-1
-accesskey=$(get_octopusvariable "TFAWSAccessKey")
-echo "Access key string is: $accesskey"
 
-echo $TFAWSAccessKey
+echo "install terraform...."
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
 
-echo "check that aws cli is working...."
+terraform --version
 
-aws s3 ls
-
-# terraform --version
