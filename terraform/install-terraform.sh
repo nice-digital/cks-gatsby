14c85ee5c76d999f356a8085f1f69fc616178f48
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-15 15:18:45 +0000

    CKS-499 More aws key diag

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -17,6 +17,9 @@ echo $TFAWSAccessKey
 export AWS_ACCESS_KEY_ID=$TFAWSAccessKey
 export AWS_SECRET_ACCESS_KEY=$TFAWSAccessSecret
 export AWS_DEFAULT_REGION=eu-west-1
+accesskey=$(get_octopusvariable "TFAWSAccessKey")
+echo "Access key string is: $accesskey"
+
 echo $TFAWSAccessKey
 
 echo "check that aws cli is working...."
