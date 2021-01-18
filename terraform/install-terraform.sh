d9604d92862ffc1752b83c0a43430593c9efa950
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-18 17:42:12 +0000

    CKS-499 Add y to apt-get

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -2,7 +2,7 @@
 echo "install useful tools...."
 
 apt-get update
-apt-get install zip unzip wget jq
+apt-get install zip unzip wget jq -y
 
 echo "install aws cli...."
 
@@ -16,7 +16,7 @@ aws --version
 echo "install terraform...."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && sudo apt-get install terraform
+apt-get update && sudo apt-get install terraform -y
 
 terraform --version
 
