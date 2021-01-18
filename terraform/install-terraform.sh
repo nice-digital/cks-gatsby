8843fc93f592d3f3f0627b1441a93f47de87f5f2
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-18 17:56:20 +0000

    CKS-499 Fixed issues

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -6,7 +6,7 @@ apt-get install zip unzip wget jq -y
 
 echo "install aws cli...."
 
-wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+wget -nv "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 unzip -q awscli-exe-linux-x86_64.zip
 ./aws/install --update
 rm awscli-exe-linux-x86_64.zip
@@ -14,7 +14,8 @@ rm awscli-exe-linux-x86_64.zip
 aws --version
 
 echo "install terraform...."
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+wget -nv -O gpg-key https://apt.releases.hashicorp.com/gpg
+apt-key add gpg-key
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && sudo apt-get install terraform -y
 
