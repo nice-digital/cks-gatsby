7a25841b98ae4231d7019290faed77f6e26191ac
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-01-18 16:57:21 +0000

    CKS-499 Removed env vars

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -12,11 +12,6 @@ unzip -q awscli-exe-linux-x86_64.zip
 
 aws --version
 
-echo "set aws cli access keys...."
-export AWS_ACCESS_KEY_ID=$(get_octopusvariable "TFAWSAccessKey")
-export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "TFAWSAccessSecret")
-export AWS_DEFAULT_REGION=eu-west-1
-
 echo "install terraform...."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
