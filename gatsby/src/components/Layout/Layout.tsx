7b53df23ea21a7ce792edb56c173b30daf7f7777
Ian Routledge <ian.routledge@nice.org.uk>
2021-01-27 20:38:14 +0000

    CKS-519 Attempt to remove CLS for font rendering

--- gatsby/src/components/Layout/Layout.tsx
+++ gatsby/src/components/Layout/Layout.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode } from "react";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import { BackToTop } from "../BackToTop/BackToTop";
-import { Lato } from "../Lato/Lato";
 
 import "./Layout.scss";
 import styles from "../BackToTop/BackToTop.module.scss";
@@ -15,7 +14,6 @@ type LayoutProps = {
 export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
 	return (
 		<>
-			<Lato />
 			<Header />
 			<main
 				className={`container ${styles.backToTopSpacing}`}
