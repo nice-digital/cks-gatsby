e93b3a963838629a1a9aa3e17fb6eba9f4a98971
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-02-05 11:58:03 +0000

    CKS-515 Script debug

--- terraform/octo-deploy-terraform.sh
+++ terraform/octo-deploy-terraform.sh
@@ -18,39 +18,39 @@ if [ -d /home/cksocto/.octopus/OctopusServer/ ]
 fi
 
 
-if [ "$runningInOctoDeploy" = true ] # Check to see if this script is runing in octo
-  then
-    echo "install terraform and support tools...."
-    chmod +x install-terraform.sh
-    sudo ./install-terraform.sh
+# if [ "$runningInOctoDeploy" = true ] # Check to see if this script is runing in octo
+#   then
+#     echo "install terraform and support tools...."
+#     chmod +x install-terraform.sh
+#     sudo ./install-terraform.sh
 
-    echo "set aws cli access keys...."
-    export AWS_ACCESS_KEY_ID=$(get_octopusvariable "TFAWSAccessKey")
-    export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "TFAWSAccessSecret")
-    export AWS_DEFAULT_REGION=eu-west-1
-    releaseNumber=$(get_octopusvariable "Octopus.Release.Number")
-    releaseEnvironment=$(get_octopusvariable "Octopus.Environment.Name")
-    searchLambdaSourceLocation=$(get_octopusvariable "[Octopus.Action[Copy Search Lambda Package].Output.Package.FilePath]")
-  else
-  dotnet lambda package CKS.SearchLambda.zip --project-location ../search-lambda/CKS.SearchLambda
-  searchLambdaSourceLocation="./CKS.SearchLambda.zip"
-  releaseEnvironment="dev"
-  releaseNumber="local-01"
-fi
+#     echo "set aws cli access keys...."
+#     export AWS_ACCESS_KEY_ID=$(get_octopusvariable "TFAWSAccessKey")
+#     export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "TFAWSAccessSecret")
+#     export AWS_DEFAULT_REGION=eu-west-1
+#     releaseNumber=$(get_octopusvariable "Octopus.Release.Number")
+#     releaseEnvironment=$(get_octopusvariable "Octopus.Environment.Name")
+#     searchLambdaSourceLocation=$(get_octopusvariable "[Octopus.Action[Copy Search Lambda Package].Output.Package.FilePath]")
+#   else
+#   dotnet lambda package CKS.SearchLambda.zip --project-location ../search-lambda/CKS.SearchLambda
+#   searchLambdaSourceLocation="./CKS.SearchLambda.zip"
+#   releaseEnvironment="dev"
+#   releaseNumber="local-01"
+# fi
 
-echo "Deploying Release Number: $releaseNumber to $releaseEnvironment"
-echo "Using lambda search source files from..... $searchLambdaSourceLocation"
-cd $releaseEnvironment
-terraform init -input=false
-terraform plan -input=false -out=tfplan -var "application_name=cks" -var "environment_name=$releaseEnvironment" -var "created_by=terraform" -var "teamcity_build_number=$releaseNumber" -var "search_lambda_source_filename=$searchLambdaSourceLocation"
-terraform apply -input=false tfplan
-echo "Current working directory is...$(pwd)"
-# aws s3 sync ../test-static-site/ s3://$(terraform output s3_hosting_bucket_id | jq -r .)
-aws s3 cp ./../test-static-site/css/* s3://$(terraform output s3_hosting_bucket_id | jq -r .)/css/ --cache-control max-age=31536000
-aws s3 cp ./../test-static-site/*.html s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=30
+# echo "Deploying Release Number: $releaseNumber to $releaseEnvironment"
+# echo "Using lambda search source files from..... $searchLambdaSourceLocation"
+# cd $releaseEnvironment
+# terraform init -input=false
+# terraform plan -input=false -out=tfplan -var "application_name=cks" -var "environment_name=$releaseEnvironment" -var "created_by=terraform" -var "teamcity_build_number=$releaseNumber" -var "search_lambda_source_filename=$searchLambdaSourceLocation"
+# terraform apply -input=false tfplan
+# echo "Current working directory is...$(pwd)"
+# # aws s3 sync ../test-static-site/ s3://$(terraform output s3_hosting_bucket_id | jq -r .)
+# aws s3 cp ./../test-static-site/css/* s3://$(terraform output s3_hosting_bucket_id | jq -r .)/css/ --cache-control max-age=31536000
+# aws s3 cp ./../test-static-site/*.html s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=30
 
-if [ "$runningInOctoDeploy" = false ]
-  then # Check to see if this script is runing in octo
-    cd ..
-    rm $searchLambdaSourceLocation
-fi
+# if [ "$runningInOctoDeploy" = false ]
+#   then # Check to see if this script is runing in octo
+#     cd ..
+#     rm $searchLambdaSourceLocation
+# fi
