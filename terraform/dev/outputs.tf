728f7ea0ce936686d6fa17f082755c1defa621f4
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-02-11 09:10:34 +0000

    CKS-515 Fixes

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -11,3 +11,8 @@ output s3_hosting_website_domain {
 	description = "The name of the bucket."
 	value = "http://${module.s3_hosting.this_s3_bucket_website_domain}"
 }
+
+output search_lambda_api_endpoint {
+	description = "The endpoint of the search api."
+	value = "${module.api_gateway.this_apigatewayv2_domain_name}"
+}
--- terraform/modules/s3_hosting/main.tf
+++ terraform/modules/s3_hosting/main.tf
@@ -54,79 +54,79 @@ resource "aws_s3_bucket_policy" "test-static-site-policy" {
 POLICY
 }
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
+resource "aws_cloudfront_distribution" "s3_distribution" {
 
-#   origin {
-#     domain_name = aws_s3_bucket.s3_website_bucket.bucket_regional_domain_name
-#     origin_id   = local.s3_origin_id
-#   }
+  origin {
+    domain_name = aws_s3_bucket.s3_website_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "CKS test static website CloudFront"
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CKS test static website CloudFront"
+  default_root_object = "index.html"
 
-# #   aliases = ["mysite.example.com", "yoursite.example.com"]
+#   aliases = ["mysite.example.com", "yoursite.example.com"]
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#     target_origin_id = local.s3_origin_id
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
-# #   price_class = "PriceClass_200"
+#   price_class = "PriceClass_200"
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     #   locations        = ["US", "CA", "GB", "DE"]
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    #   locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
 
-#   tags = {
-# 	application_name = var.application_name
-# 	created_by = var.created_by
-# 	environment_name = var.environment_name
-# 	teamcity_build_number = var.teamcity_build_number
-#   }
+  tags = {
+	application_name = var.application_name
+	created_by = var.created_by
+	environment_name = var.environment_name
+	teamcity_build_number = var.teamcity_build_number
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
