17c01279dee67e72a099fd9f016227eede4773a3
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-02-23 16:26:07 +0000

    CKS-525 Change to lamda tp request lambda

--- edge-lambda/index.js
+++ edge-lambda/index.js
@@ -1,12 +1,7 @@
-"use strict";
 exports.handler = (event, context, callback) => {
-	//Get contents of response
-	const response = event.Records[0].cf.response;
-	const headers = response.headers;
-
-	//Set new headers
-	headers["x-nice-test1"] = [{ key: "X-Nice-Test1", value: "Hello from nice" }];
-
-	//Return modified response
-	callback(null, response);
+	const redirectResponse = {
+		status: "403",
+		statusDescription: "Forbidden",
+	};
+	callback(null, redirectResponse);
 };
