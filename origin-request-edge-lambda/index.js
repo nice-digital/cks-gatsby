62827c516331fde97373acafa9cb585cc602348c
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-02 11:42:31 +0000

    CKS-533 Refactor country allowlist

--- origin-request-edge-lambda/index.js
+++ origin-request-edge-lambda/index.js
@@ -5,20 +5,44 @@ const redirects = require("./redirects.json").map(
 	})
 );
 
-exports.handler = async (event) => {
-	const request = event.Records[0].cf.request;
+const allowedCounties = require("./allowedCounties.json").map(
+	(country) => country
+);
+
+exports.handler = (event, context, callback) => {
+	let request = event.Records[0].cf.request;
+	const countryHeader = request.headers["cloudfront-viewer-country"];
+	const countryCode = countryHeader[0].value;
+
+	if (!isInAllowList(countryCode)) {
+		//if request is from non UK country
+		const redirectResponse = {
+			status: "403",
+			statusDescription: "Forbidden",
+		};
+		callback(null, redirectResponse);
+	}
 
 	for (const { source, destination } of redirects) {
 		if (source.test(request.uri)) {
-			return {
+			//if uri matches entry in rewrite list return modified uri
+			const redirectResponse = {
 				status: "302",
 				statusDescription: "Found",
 				headers: {
 					location: [{ value: request.uri.replace(source, destination) }],
 				},
 			};
+			callback(null, redirectResponse);
 		}
 	}
 
-	return request;
+	callback(null, request);
 };
+
+function isInAllowList(countryCode) {
+	if (allowedCounties.includes(countryCode)) {
+		return true;
+	}
+	return false;
+}
