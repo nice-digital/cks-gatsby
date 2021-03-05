add0f30243f932394d4cc170bdd4f15120ade457
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-05 11:40:41 +0000

    CKS-550 Comment out s3

--- terraform/deploy-static-to-s3.sh
+++ terraform/deploy-static-to-s3.sh
@@ -3,8 +3,8 @@
 # eg deploy-static-to-s3.sh nice-cks-local-s3-web-hosting 123
 echo "Deploying files to S3 bucket: $1"
 
-aws s3 cp ./../../gatsby/public/ s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=31536000 --exclude "*.html" --exclude "*.json" --recursive
-aws s3 cp ./../../gatsby/public/ s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=31536000 --exclude "*.html" --exclude "*.json" --recursive
+# aws s3 cp ./../../gatsby/public/ s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=31536000 --exclude "*.html" --exclude "*.json" --recursive
+# aws s3 cp ./../../gatsby/public/ s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=31536000 --exclude "*.html" --exclude "*.json" --recursive
 
 # aws s3 cp ./../test-static-site/css/* s3://$(terraform output s3_hosting_bucket_id | jq -r .)/css/ --cache-control max-age=31536000
 # aws s3 cp ./../test-static-site/*.html s3://$(terraform output s3_hosting_bucket_id | jq -r .)/ --cache-control max-age=30
