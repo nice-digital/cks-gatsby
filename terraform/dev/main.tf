1c873dc6627582e34ae71ce1ee6aa66b0fbc9fac
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-05 15:50:13 +0000

    CKS-550 Update s3 state for dev

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,11 +1,11 @@
 terraform {
 	required_version = ">= 0.14"
-	# backend "s3" {
-	# 	encrypt = true
-	# 	bucket = "#{TerraformStateBucketName}"
-	# 	region = "eu-west-1"
-	# 	key = "nice/cks/#{Octopus.Environment.Name}"
-	# }
+	backend "s3" {
+		encrypt = true
+		bucket = "#{TerraformStateBucketName}"
+		region = "eu-west-1"
+		key = "nice/cks/#{Octopus.Environment.Name}"
+	}
 }
 
 ################################################################################
@@ -78,43 +78,42 @@ module "cf_hosting" {
   source = "../modules/cf_hosting"
 
   name = local.name
-  origin_request_edge_lambda_qualified_arn = module.origin_request_edge_lambda.this_lambda_qualified_arn
-  origin_repsonse_edge_lambda_qualified_arn = module.origin_response_edge_lambda.this_lambda_qualified_arn
-  viewer_request_edge_lambda_qualified_arn = module.viewer_request_edge_lambda.this_lambda_qualified_arn
-
-
-  tags = local.default_tags
-}
-
-module "origin_request_edge_lambda" {
-  source = "../modules/edge_lambda"
-
-  lambda_source_filename = var.origin_request_edge_lambda_source_filename
-
-  name = local.name
-  event_type = "origin-request"
+#   origin_request_edge_lambda_qualified_arn = module.origin_request_edge_lambda.this_lambda_qualified_arn
+#   origin_repsonse_edge_lambda_qualified_arn = module.origin_response_edge_lambda.this_lambda_qualified_arn
+#   viewer_request_edge_lambda_qualified_arn = module.viewer_request_edge_lambda.this_lambda_qualified_arn
 
   tags = local.default_tags
 }
 
-module "origin_response_edge_lambda" {
-  source = "../modules/edge_lambda"
+# module "origin_request_edge_lambda" {
+#   source = "../modules/edge_lambda"
 
-  lambda_source_filename = var.origin_response_edge_lambda_source_filename
+#   lambda_source_filename = var.origin_request_edge_lambda_source_filename
 
-  name = local.name
-  event_type = "origin-response"
+#   name = local.name
+#   event_type = "origin-request"
 
-  tags = local.default_tags
-}
+#   tags = local.default_tags
+# }
 
-module "viewer_request_edge_lambda" {
-  source = "../modules/edge_lambda"
+# module "origin_response_edge_lambda" {
+#   source = "../modules/edge_lambda"
 
-  lambda_source_filename = var.viewer_request_edge_lambda_source_filename
+#   lambda_source_filename = var.origin_response_edge_lambda_source_filename
 
-  name = local.name
-  event_type = "viewer-request"
+#   name = local.name
+#   event_type = "origin-response"
 
-  tags = local.default_tags
-}
+#   tags = local.default_tags
+# }
+
+# module "viewer_request_edge_lambda" {
+#   source = "../modules/edge_lambda"
+
+#   lambda_source_filename = var.viewer_request_edge_lambda_source_filename
+
+#   name = local.name
+#   event_type = "viewer-request"
+
+#   tags = local.default_tags
+# }
--- terraform/modules/cf_hosting/variables.tf
+++ terraform/modules/cf_hosting/variables.tf
@@ -6,15 +6,15 @@ variable "name" {
 	type = string
 }
 
-variable "origin_request_edge_lambda_qualified_arn" {
-	type = string
-}
-variable "origin_repsonse_edge_lambda_qualified_arn" {
-	type = string
-}
-variable "viewer_request_edge_lambda_qualified_arn" {
-	type = string
-}
+# variable "origin_request_edge_lambda_qualified_arn" {
+# 	type = string
+# }
+# variable "origin_repsonse_edge_lambda_qualified_arn" {
+# 	type = string
+# }
+# variable "viewer_request_edge_lambda_qualified_arn" {
+# 	type = string
+# }
 
 variable "tags" {
   description = "Tags"
