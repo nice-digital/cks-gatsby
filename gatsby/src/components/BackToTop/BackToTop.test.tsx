f836d40509428ea229bfa359351d98124a661036
Ian Routledge <ian.routledge@nice.org.uk>
2021-03-08 10:19:59 +0000

    CKS-542 Remove layout shift for back to top button (#149)
    * CKS-542 Remove layout shift for back to top button
    
    Change back to top to be CSS only rather than JS
    
    * CKS-542 Update NDS version
    
    * CKS-542 Bump global nav
    
    * CKS-542 Fix CSS loading
    
    * CKS-542 Fix  mutation observer hack
    
    * CKS-542 Fix mutation observer bug
    
    * CKS-542 Fix back to top positioning when zoomed
    
    * CKS-542 Fix IE11 bug
    
    * CKS-542 Bump global nav to master commit

--- gatsby/src/components/BackToTop/BackToTop.test.tsx
+++ gatsby/src/components/BackToTop/BackToTop.test.tsx
@@ -7,15 +7,4 @@ describe("BackToTop", () => {
 	it("should match snapshot", () => {
 		expect(render(<BackToTop />).container).toMatchSnapshot();
 	});
-
-	it("should have a class of static on first render", () => {
-		const { container } = render(<BackToTop />);
-		expect(container.querySelectorAll(".static")).toHaveLength(1);
-	});
-
-	it("should have a class of fixed when the page is scrolled", () => {
-		const renderResult = render(<BackToTop />);
-		fireEvent.scroll(window, { target: { scrollY: 810 } });
-		expect(renderResult.container.querySelectorAll(".static")).toHaveLength(1);
-	});
 });
