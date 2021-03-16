c396666347dd6832555d9f05c3442d34aad50945
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-16 15:59:37 +0000

    CKS-550 Remove response

--- edge-lambdas/origin-response-edge-lambda/index.js
+++ edge-lambdas/origin-response-edge-lambda/index.js
@@ -1,33 +1,3 @@
-exports.handler = async (event, context) => {
-	const response = event.Records[0].cf.response;
-	const headers = response.headers;
-
-	headers["content-security-policy"] = [
-		{
-			key: "Content-Security-Policy",
-			value: "frame-ancestors 'self'",
-		},
-	];
-	headers["x-content-type-options"] = [
-		{ key: "X-Content-Type-Options", value: "nosniff" },
-	];
-	headers["x-frame-options"] = [
-		{ key: "X-Frame-Options", value: "SAMEORIGIN" },
-	];
-	headers["x-xss-protection"] = [
-		{ key: "X-XSS-Protection", value: "1; mode=block" },
-	];
-	headers["referrer-policy"] = [
-		{ key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-	];
-	headers["link"] = [
-		{
-			key: "link",
-			value:
-				"<https://apikeys.civiccomputing.com>; rel=preconnect; crossorigin,<https://www.googletagmanager.com>; rel=preconnect,<https://cdn.nice.org.uk/cookie-banner/cookie-banner.min.js>; rel=preload; as=script,</fonts/lato-v17-latin-regular.woff2>; rel=preload; as=font; crossorigin=anonymous,</fonts/lato-v17-latin-italic.woff2>; rel=preload; as=font; crossorigin=anonymous,</fonts/lato-v17-latin-700.woff2>; rel=preload; as=font; crossorigin=anonymous,</fonts/lato-v17-latin-900.woff2>; rel=preload; as=font; crossorigin=anonymous",
-			// "<https://apikeys.civiccomputing.com>; rel=preconnect; crossorigin,<https://www.googletagmanager.com>; rel=preconnect,<https://cdn.nice.org.uk/cookie-banner/cookie-banner.min.js>; rel=preload; as=script",
-		},
-	];
-
-	return response;
+exports.handler = (event, context, callback) => {
+	callback(null, response);
 };
