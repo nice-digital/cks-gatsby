a5f82e7a5b7d59b055ff2f4499c0483c9c29933f
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-19 14:00:01 +0000

    CKS-531 Add allow list header to lambda

--- edge-lambdas/viewer-request-edge-lambda/index.js
+++ edge-lambdas/viewer-request-edge-lambda/index.js
@@ -1,3 +1,31 @@
+var Netmask = require("netmask").Netmask;
+
+const ipAllowList = require("./ipAllowList.json");
+
 exports.handler = (event, context, callback) => {
+	let request = event.Records[0].cf.request;
+	const headers = request.headers;
+
+	if (isInIpAllowList(request.clientIp)) {
+		//if request is in ip allow list
+		headers["x-nice-allowed-ip"] = [
+			{
+				key: "x-nice-allowed-ip",
+				value: request.clientIp,
+			},
+		];
+	}
 	callback(null, request);
 };
+
+function isInIpAllowList(sourceIpAddress) {
+	var netBlockArray = ipAllowList.map((addressMask) => {
+		const block = new Netmask(addressMask);
+		if (block.contains(sourceIpAddress)) {
+			return true;
+		}
+		return false;
+	});
+
+	return netBlockArray.includes(true);
+}
