a5ec5a1e329d930a6ad86b8f0aadab634316fb6f
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-23 19:46:44 +0000

    CKS-550 Update viewer req lambda

--- edge-lambdas/viewer-request-edge-lambda/index.js
+++ edge-lambdas/viewer-request-edge-lambda/index.js
@@ -5,6 +5,7 @@ const ipAllowList = require("./ipAllowList.json");
 exports.handler = (event, context, callback) => {
 	let request = event.Records[0].cf.request;
 	const headers = request.headers;
+	console.log("Input request " + JSON.stringify(request));
 
 	if (isInIpAllowList(request.clientIp)) {
 		//if request is in ip allow list
@@ -15,13 +16,16 @@ exports.handler = (event, context, callback) => {
 			},
 		];
 	}
+	console.log("Out request " + JSON.stringify(request));
 	callback(null, request);
 };
 
 function isInIpAllowList(sourceIpAddress) {
+	console.log(sourceIpAddress);
 	var netBlockArray = ipAllowList.map((addressMask) => {
 		const block = new Netmask(addressMask);
 		if (block.contains(sourceIpAddress)) {
+			console.log(sourceIpAddress - "true");
 			return true;
 		}
 		return false;
