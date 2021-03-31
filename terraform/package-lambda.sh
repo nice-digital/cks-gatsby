db0707a3c14b672363486950f1177f4462de8e09
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-31 14:40:27 +0100

    CKS-534 Remove zip metadata

--- terraform/package-lambda.sh
+++ terraform/package-lambda.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo packaging $1
 cd ../edge-lambdas/$1/
-touch -a -m -t 200001010000.00 *  # Remvoe the timestamp to only detect actual changes
+touch -a -m -t 200001010000.00 *  # Remove the timestamp to only detect actual changes
 rm -f ../../terraform/lambdas/$1.zip
 zip -r -X ../../terraform/lambdas/$1.zip .
+../../terraform/stripzip ../../terraform/lambdas/$1.zip # Remove zip metadata
