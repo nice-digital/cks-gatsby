99ecd5b9425847aa1f034fb36718b26fef43ffda
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-03-31 12:30:53 +0100

    CKS-534 Fix issues with zip file changes

--- terraform/package-lambda.sh
+++ terraform/package-lambda.sh
@@ -2,4 +2,5 @@
 echo packaging $1
 cd ../edge-lambdas/$1/
 touch -a -m -t 200001010000.00 *  # Remvoe the timestamp to only detect actual changes
-zip -r ../../terraform/lambdas/$1.zip .
+rm -f ../../terraform/lambdas/$1.zip
+zip -r -X ../../terraform/lambdas/$1.zip .
