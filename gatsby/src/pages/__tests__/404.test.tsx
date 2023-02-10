a2408e911c37d57c4b26dafaac144e5295e88199
David Wong <david.wong@nice.org.uk>
2023-02-10 09:34:40 +0000

    remove console log

--- gatsby/src/pages/__tests__/404.test.tsx
+++ gatsby/src/pages/__tests__/404.test.tsx
@@ -7,7 +7,6 @@ import NotFoundPage from "../404";
 describe("404", () => {
 	it("should match snapshot", () => {
 		const { container } = renderWithRouter(<NotFoundPage />);
-		console.log(container.innerHTML);
 		expect(container).toMatchSnapshot();
 	});
 
