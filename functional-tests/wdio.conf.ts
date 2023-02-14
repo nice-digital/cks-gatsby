99717c826a97d256bc1b2e24ce82590a15e2e442
David Wong <david.wong@nice.org.uk>
2023-02-14 14:48:19 +0000

    maxINstances to 1

--- functional-tests/wdio.conf.ts
+++ functional-tests/wdio.conf.ts
@@ -7,7 +7,7 @@ export const config: WebdriverIO.Config = {
 	// We need to use webdriver protocol in Docker because we use the selenium grid.
 	automationProtocol: isInDocker ? "webdriver" : "devtools",
 
-	maxInstances: isInDocker ? 2 : 2,
+	maxInstances: isInDocker ? 1 : 2,
 	path: "/wd/hub",
 
 	specs: ["./features/**/*.feature"],
