9089711e10ae100f9555255f4b018a7f89b14105
lizaadebowale <liza.adebowale@nice.org.uk>
2023-06-05 11:48:30 +0100

    CKS-694 Fix TC build error

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-sitemap`,
 			options: {
-				excludes: [`/search/`]				
+				excludes: [`/search/`],				
 			},
 		},
 		"gatsby-plugin-react-helmet",
