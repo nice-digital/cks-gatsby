a61b312ea44672f57ca2e19d144e93395cd2d2e2
John Davey <john.davey@nice.org.uk>
2024-01-03 09:47:35 +0000

    CKS-713 Add robots txt to prevent generative AI crawlers

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -21,6 +21,19 @@ module.exports = {
 	},
 	plugins: [
 		{
+			resolve: "gatsby-plugin-robots-txt",
+			options: {
+				host: siteUrl,
+				sitemap: `${siteUrl}/sitemap/sitemap-index.xml`,
+				policy: [
+					{ userAgent: "GPTBot", disallow: "/" },
+					{ userAgent: "ChatGPT-User", disallow: "/" },
+					{ userAgent: "Google-Extended", disallow: "/" },
+					{ userAgent: "CCBot", disallow: "/" },
+				],
+			},
+		},
+		{
 			resolve: `gatsby-plugin-sitemap`,
 			options: {
 				excludes: [`/search/`],
