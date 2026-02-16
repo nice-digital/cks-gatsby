e7aa5dabae19d318e932f4544f09d08340101cbf
Shilpa Unni <shilpa.unni@nice.org.uk>
2026-02-16 18:19:54 +0000

    CKS-806 Enhance search results announcement timing and focus for improved accessibility

--- gatsby/src/pages/search.tsx
+++ gatsby/src/pages/search.tsx
@@ -83,11 +83,6 @@ const SearchPage: React.FC = () => {
 			.then((results) => {
 				setError(false);
 				setData(results as SearchResults);
-				announce(
-					`${results.resultCount} ${
-						results.resultCount === 1 ? "result" : "results"
-					}${results.finalSearchText ? ` for ${results.finalSearchText}` : ""}`
-				);
 			})
 			.catch(() => {
 				setError(true);
@@ -117,6 +112,13 @@ const SearchPage: React.FC = () => {
 		if (!data?.documents?.length || error) return;
 
 		resultsRef.current?.focus({ preventScroll: true });
+		window.setTimeout(() => {
+			announce(
+				`${data.resultCount} ${data.resultCount === 1 ? "result" : "results"}${
+					data.finalSearchText ? ` for ${data.finalSearchText}` : ""
+				}`
+			);
+		}, 100);
 	}, [data?.documents?.length, error]);
 
 	return (
