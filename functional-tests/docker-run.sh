285f6d3f52e119df44078a9f3f1ff1aa40edb6d7
John Davey <john.davey@nice.org.uk>
2026-07-28 17:50:16 +0100

    CKS-830 Reduce wdio resources as it is breaking teamcity

--- functional-tests/docker-run.sh
+++ functional-tests/docker-run.sh
@@ -68,7 +68,7 @@ catch() {
 }
 
 cleanupBeforeStart
-docker-compose up -d --build --scale selenium-chrome=5
+docker-compose up -d --build --scale selenium-chrome=2
 runTests
 processTestOutput
 cleanup
